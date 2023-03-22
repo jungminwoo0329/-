9845a8fdfc7e4a74a19aa6940b5142d1f966aa01
jungminwoo0329 <93774389+jungminwoo0329@users.noreply.github.com>
2023-03-22 14:47:57 +0900

    chap2

---
+++ ttt1.py
@@ -0,0 +1,11 @@
+#원의 면적을 구하는 프로그램
+
+radius = 10
+
+area = 3.14 * radius * radius
+
+print(f"반지름이 {radius}인 원의 면적은 {area} 입니다")
+
+# print("반지름이", radius, "인 원의 면적은", area, "입니다")
+
+# print("반지름이" + str(radius) + "인 원의 면적은" + str(area) + "입니다")
