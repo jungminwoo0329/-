b3a1a57c369a4e4d9d748dedc9b803026cafc7d8
jungminwoo0329 <93774389+jungminwoo0329@users.noreply.github.com>
2023-03-22 15:48:25 +0900

    chap2

---
+++ ttt2.py
@@ -0,0 +1,42 @@
+x="영이공"
+y="1학년"
+
+print(x,y)
+print(x + y)
+
+
+
+
+
+# profile = '''성명 : 정민우
+# 학교 : 영이공
+# 학년 : 1학년
+# 취미 : 코딩'''
+
+# print(profile)
+
+
+
+
+# x=3.5
+# y=3.5e4
+# z=3.5e-7
+# print(x,y,z)
+
+
+
+
+
+
+# x= 17
+# print(bin(x),oct(x),hex(x))
+
+
+
+
+# x=11
+# y=0b101
+# z=0o101
+# a=0x101
+
+# print(x,y,z,a)
